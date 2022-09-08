0f8b1edbbc975a51d93c1f7a87b07ec24e280e17
myaltaccountsthis <71832770+myaltaccountsthis@users.noreply.github.com>
2022-09-08 18:02:17 -0500

    Update WordLadder.java

--- LabWordLadder/src/WordLadder.java
+++ LabWordLadder/src/WordLadder.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 import java.util.*;
 
 public class WordLadder {
@@ -15,85 +17,100 @@ public class WordLadder {
         input.close();
     }
 
-    private String getErrorMessage(String start, String end) {
-        return "No ladder between " + start + " and " + end;
-    }
+    private class Solver {
+        private final String start, end;
+        private boolean ran = false, success = false;
+        private Stack<String> solution = null;
 
-    private String getSuccessMessage(Stack<String> stack) {
-        return "Found a ladder! >>> " + stack;
-    }
+        private final Collection<String> validWords = new HashSet<>();
+        //private final HashMap<String, Integer> wordsRequired = new HashMap<>();
+        private final Queue<Stack<String>> queue = new LinkedList<>();
 
-    /**
-     * Return all words that differ by one letter from {@link String word}, if they are not in {@link ArrayList<String> ignoreList}
-     * @param word
-     * @return
-     */
-    private Queue<String> getAdjacentWords(String word, Collection<String> validWords) {
-        Queue<String> words = new LinkedList<>();
-        // third optimization, the fastest way to get all adjacent chars (26 * word.length(), e.g. 130 for 5 letter word)
-        // compare to looping through all in validWords and checking each one's character difference (e.g. 8.4k for 5 letter word)
-        for (int i = 0; i < word.length(); i++) {
-            for (char c = 'a'; c <= 'z'; c++) {
-                // don't need to check if s.equals(word) bc validWords.contains(word) will return false
-                String s = word.substring(0, i) + c + word.substring(i + 1);
-                if (validWords.contains(s))
-                    words.offer(s);
-            }
+        public Solver(String start, String end) {
+            this.start = start;
+            this.end = end;
         }
-        return words;
-    }
 
-    public Stack<String> solve(String start, String end) {
-        if (start.length() != end.length())
-            return null;
+        private String getErrorMessage() {
+            return "No ladder between " + start + " and " + end;
+        }
 
-        Collection<String> validWords = new HashSet<>();
-        HashMap<String, Integer> wordsRequired = new HashMap<>();
-        Queue<Stack<String>> queue = new LinkedList<>();
+        private String getSuccessMessage(Stack<String> stack) {
+            return "Found a ladder! >>> " + stack;
+        }
 
-        { // setup
-            for (String s : dictionary)
-                if (s.length() == start.length())
-                    validWords.add(s);
-            if (!validWords.contains(start) || !validWords.contains(end))
+        public String getMessage() {
+            return !ran ? "Ladder not checked" : success && solution != null ? getSuccessMessage(solution) : getErrorMessage();
+        }
+
+        /**
+         * Return all words that differ by one letter from {@link String word}, if they are not in {@link ArrayList<String> ignoreList}
+         *
+         * @param word
+         * @return
+         */
+        private Collection<String> getAdjacentWords(String word) {
+            Collection<String> words = new ArrayList<>();
+            // optimization, the fastest way to get all adjacent chars (26 * word.length(), e.g. 130 for 5 letter word)
+            // compare to looping through all in validWords and checking each one's character difference (e.g. 8.4k for 5 letter word)
+            for (int i = 0; i < word.length(); i++) {
+                for (char c = 'a'; c <= 'z'; c++) {
+                    // don't need to check if s.equals(word) bc validWords.contains(word) will return false
+                    String s = word.substring(0, i) + c + word.substring(i + 1);
+                    if (validWords.contains(s))
+                        words.add(s);
+                }
+            }
+            return words;
+        }
+
+        private void onSuccess(Stack<String> s) {
+            this.success = true;
+            this.solution = s;
+        }
+
+        public Stack<String> solve() {
+            assert !ran : "Solver already ran";
+            ran = true;
+
+            if (start.length() != end.length())
                 return null;
-            if (start.equals(end))
-                return new Stack<>() {{ push(start); }};
-            Queue<String> adjacent = getAdjacentWords(start, validWords);
-            while (!adjacent.isEmpty()) {
-                Stack<String> s = new Stack<>();
-                s.push(start);
-                s.push(adjacent.poll());
-                queue.offer(s);
-            }
-        }
 
-        Stack<String> shortest = null;
-        while (!queue.isEmpty()) {
-            Stack<String> stack = queue.poll();
-            validWords.remove(stack.peek());
-            if (shortest != null && stack.size() >= shortest.size())
-                continue;
-            if (stack.peek().equals(end)) {
-                // first optimization, check if this stack is shorter bc if it is >= then it cannot be shorter
-                if (shortest == null || stack.size() < shortest.size())
-                    shortest = stack;
-                if (shortest.size() <= 2)
-                    return shortest;
+            { // setup
+                for (String s : dictionary)
+                    if (s.length() == start.length())
+                        validWords.add(s);
+                if (!validWords.contains(start) || !validWords.contains(end))
+                    return null;
+                queue.offer(new Stack<>() {{ push(start); }});
+                validWords.remove(start);
             }
-            Queue<String> adjacent = getAdjacentWords(stack.peek(), validWords);
-            while (!adjacent.isEmpty()) {
-                Stack<String> s = new Stack<>() {{ addAll(stack); }};
-                s.push(adjacent.poll());
-                // second optimization, check if the number of words to get to s.peek() is shorter than the recorded shortest value, or Integer.MAX_VALUE if there is none
-                if (s.size() < wordsRequired.getOrDefault(s.peek(), Integer.MAX_VALUE)) {
-                    wordsRequired.put(s.peek(), s.size());
+
+            Stack<String> shortest = null;
+            while (!queue.isEmpty()) {
+                Stack<String> stack = queue.poll();
+                if (stack.peek().equals(end)) {
+                    shortest = stack;
+                    break;
+                }
+                Collection<String> adjacent = getAdjacentWords(stack.peek());
+
+                for (String str : adjacent) {
+                    Stack<String> s = new Stack<>() {{ addAll(stack); push(str); }};
+                    validWords.remove(str);
                     queue.offer(s);
                 }
             }
-        }
 
-        return shortest;
+            onSuccess(shortest);
+            return shortest;
+        }
+    }
+
+    public Solver solve(String start, String end) {
+        Solver solver = new Solver(start, end);
+        solver.solve();
+        return solver;
     }
 
     public void solveFile(String fileName) throws FileNotFoundException {
@@ -103,8 +120,9 @@ public class WordLadder {
             String line = input.nextLine();
             Scanner scanner = new Scanner(line);
             String start = scanner.next(), end = scanner.next();
-            Stack<String> solution = solve(start, end);
-            System.out.println(solution == null ? getErrorMessage(start, end) : getSuccessMessage(solution));
+            long solve = System.currentTimeMillis();
+            Solver solver = solve(start, end);
+            System.out.println((System.currentTimeMillis() - solve) + "\t: " + solver.getMessage());
             scanner.close();
         }
         System.out.println("Took " + (System.currentTimeMillis() - t) + " ms");
