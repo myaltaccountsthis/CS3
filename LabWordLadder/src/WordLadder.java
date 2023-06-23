22c22c89cc01b4241f82b6d8dec674a9d9fcc249
myaltaccountsthis <71832770+myaltaccountsthis@users.noreply.github.com>
2023-06-23 13:01:31 -0500

    wordladder a*

--- LabWordLadder/src/WordLadder.java
+++ LabWordLadder/src/WordLadder.java
@@ -107,8 +107,121 @@ public class WordLadder {
         }
     }
 
+    private class SolverBetter {
+        private final String start, end;
+        private boolean ran = false;
+        private List<String> solution = null;
+        long adjTime = 0;
+
+        private final Collection<String> validWords = new HashSet<>();
+        private final Map<String, String> parents = new HashMap<>();
+        private final Map<String, Integer> costs = new HashMap<>();
+        private final Map<String, Integer> heuristics = new HashMap<>();
+        private final PriorityQueue<String> pq = new PriorityQueue<>(Comparator.comparingInt(s -> costs.get(s) + heuristics.get(s)));
+
+        public SolverBetter(String start, String end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        public String getMessage() {
+            if (!ran)
+                return "Ladder not checked";
+            if (solution != null)
+                return "Found a ladder! >>> " + solution;
+            return "No ladder between " + start + " and " + end;
+        }
+
+        /**
+         * Return all words that differ by one letter from {@code String word}, if they are not in {@code ArrayList<String> ignoreList}
+         *
+         * @param word
+         * @return
+         */
+        private Collection<String> getAdjacentWords(String word) {
+            long t = System.nanoTime();
+            Collection<String> words = new ArrayList<>();
+            // optimization, the fastest way to get all adjacent chars (26 * word.length(), e.g. 130 for 5 letter word)
+            // compare to looping through all in validWords and checking each one's character difference (e.g. 8.4k for 5 letter word)
+            char[] arr = word.toCharArray();
+            for (int i = 0; i < word.length(); i++) {
+                char original = word.charAt(i);
+                for (char c = 'a'; c <= 'z'; c++) {
+                    // using char array is faster bc string concatenation substring is slow
+                    arr[i] = c;
+                    String s = new String(arr);
+                    // don't need to check if s.equals(word) bc validWords.contains(word) will return false
+                    if (validWords.contains(s))
+                        words.add(s);
+                }
+                arr[i] = original;
+            }
+            adjTime += System.nanoTime() - t;
+            return words;
+        }
+
+        private int h(String word) {
+            int err = 0;
+            for (int i = 0; i < word.length(); i++) {
+                if (word.charAt(i) != end.charAt(i))
+                    err++;
+            }
+            return err;
+        }
+
+        public void solve() {
+            assert !ran : "Solver already ran";
+            ran = true;
+
+            if (start.length() != end.length())
+                return;
+
+            { // setup
+                for (String s : dictionary)
+                    if (s.length() == start.length())
+                        validWords.add(s);
+                if (!validWords.contains(start) || !validWords.contains(end))
+                    return;
+                parents.put(start, null);
+                costs.put(start, 0);
+                heuristics.put(start, h(start));
+                pq.offer(start);
+                validWords.remove(start);
+            }
+
+            while (!pq.isEmpty()) {
+                String current = pq.poll();
+                if (current.equals(end)) {
+                    break;
+                }
+                Collection<String> adjacent = getAdjacentWords(current);
+                for (String str : adjacent) {
+                    // this only runs once per word
+                    parents.put(str, current);
+                    costs.put(str, costs.get(current) + 1);
+                    heuristics.put(str, h(str));
+                    pq.offer(str);
+                    validWords.remove(str);
+                }
+            }
+
+            if (parents.containsKey(end)) {
+                LinkedList<String> list = new LinkedList<>();
+                for (String word = end; word != null; word = parents.get(word))
+                    list.addFirst(word);
+                this.solution = list;
+            }
+        }
+    }
+
     public Solver solve(String start, String end) {
         Solver solver = new Solver(start, end);
+        solver.solve();
+        return solver;
+    }
+
+    public SolverBetter solveImproved(String start, String end) {
+        SolverBetter solver = new SolverBetter(start, end);
         solver.solve();
         return solver;
     }
@@ -119,11 +232,18 @@ public class WordLadder {
         BufferedReader reader = new BufferedReader(new FileReader(fileName));
         Iterable<String> lines = reader.lines().toList();
         for (String line : lines) {
-            long solve = System.nanoTime();
             String[] tokens = line.split(" ");
             String start = tokens[0], end = tokens[1];
-            Solver solver = solve(start, end);
-            System.out.println((System.nanoTime() - solve) / 1000000 + "\t\t" + solver.adjTime / 1000000 + "\t\t: " + solver.getMessage());
+            {
+                long solve = System.nanoTime();
+                Solver solver = solve(start, end);
+                System.out.printf("%d\t\t%d\t\t%s%n", (System.nanoTime() - solve) / 1000000, solver.adjTime / 1000000, solver.getMessage());
+            }
+            {
+                long solve = System.nanoTime();
+                SolverBetter solver = solveImproved(start, end);
+                System.out.printf("%d\t\t%d\t\t%s%n", (System.nanoTime() - solve) / 1000000, solver.adjTime / 1000000, solver.getMessage());
+            }
         }
         System.out.println("Took " + (System.nanoTime() - t) / 1000000 + " ms to solve file");
     }
